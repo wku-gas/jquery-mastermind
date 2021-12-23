0d6c58f1e4b3b0880860a8e6a05d2963073e769f
William Ku <kucheech@gmail.com>
2021-12-23 12:00:00 +0800

    add logic to generate and check answers

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+const colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const getRandomColour = () => colours[Math.floor(Math.random() * colours.length)];
+const generateAnswer = () => [getRandomColour(), getRandomColour(), getRandomColour(), getRandomColour()];
+const checkAnswers = (guess, answer) => {
+  let blacks = 0, whites = 0;
+
+  for (let i = 0; i < 4; i++) {
+    const a = answer[i];
+    if (a === guess[i]) {
+      blacks++;
+      answer[i] = null;
+      guess[i] = null;
+    }
+  }
+
+  for (let i = 0; i < 4; i++) {
+    const a = answer[i];
+    if (a && guess.includes(a)) {
+      whites++;
+    }
+  }
+
+  return [blacks, whites];
+}
+
 $(() => {
-  console.log($("body"));
+  console.log(checkAnswers(['red', 'red', 'red', 'blue'], ['red', 'red', 'blue', 'blue']))
 });
