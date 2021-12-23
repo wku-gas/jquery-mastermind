df1fa63c479390d35a7598d17f05433b0f9337a9
William Ku <kucheech@gmail.com>
2021-12-23 17:39:26 +0800

    * add game mechanism to guess on clicking the clue pegs * set max attempts to 5 and display an Game Over alert

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let answer = generateAnswer()
+const answer = generateAnswer()
+const maxAttempts = 5
+let attempts = 0
 
 const getPegColour = $peg => {
   const colour = $.Color($peg, 'background-color')
@@ -11,13 +13,60 @@ const getColourName = colour => {
 }
 
 const getGuess = () => {
-  return $('.peg').toArray().map(p => getColourName(p.style?.backgroundColor))
+  return $('.attempt .peg').toArray().map(p => getColourName(p.style?.backgroundColor))
 }
 
-$(() => {
-  $('.peg').click(function () {
+const addRow = () => {
+  $('.board').append(`
+  <div class="row attempt">
+      <div class="pegs">
+        <div class="peg"></div>
+        <div class="peg"></div>
+        <div class="peg"></div>
+        <div class="peg"></div>
+      </div>
+      <div class="cluepegs">
+        <div class="cluepeg"></div>
+        <div class="cluepeg"></div>
+        <div class="cluepeg"></div>
+        <div class="cluepeg"></div>
+      </div>
+    </div>
+  `)
+}
+
+const disablePreviousAttempt = () => {
+  $('.attempt .peg').off('click')
+  $('.attempt .cluepegs').off('click')
+
+  $('.attempt').removeClass('attempt')
+}
+
+const addAttempt = () => {
+  disablePreviousAttempt()
+
+  addRow()
+
+  $('.attempt .peg').click(function () {
     $(this).css('background-color', getNextColour(getPegColour($(this))))
   })
 
+  $('.attempt .cluepegs').click(function () {
+    const guess = getGuess()
+    if (!guess.includes(undefined)) {
+      if (attempts < maxAttempts) {
+        addAttempt()
+      } else {
+        alert('Game Over ...')
+      }
+    }
+  })
+
+  attempts++
+}
+
+$(() => {
+
+  addAttempt()
 
 });
